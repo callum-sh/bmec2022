548b68a014d9bbb240e808db374e97fc0f1c537e
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2022-02-20 00:53:48 -0500

    updating UI changes

--- to-do.py
+++ to-do.py
@@ -1,65 +1,41 @@
 import tkinter as tk
 from tkinter import StringVar, messagebox
+from tkinter import *
 import tkinter.font as tkFont
+from turtle import color
+import platform
 
 app = tk.Tk()
+
+app.title('BMEC2022: Power Ranger To-Do Demo')
 
 NUM_TASKS = 14
 i = 0
 num = 0
 
-app.geometry("900x500")
-
 tsk = StringVar()
-
 desc = StringVar()
 
-def t0():
-    return "Wash, brush teeth, get dressed"
+app.geometry("900x500")
+fontStyle = tkFont.Font(family="Open Sans", size = 30)
 
-def t1():
-    return "Prepare and eat breakfast"
+tsk.set("Click 'Finish Task' to Start")
 
-def t2():
-    return "Have a conversation over coffee"
-
-def t3():
-    return "Discuss the newspaper, try a craft project, reminisce about old photos"
-
-def t4():
-    return "Take a break, have some quiet time"
-
-def t5():
-    return "Do some chores together"
-
-def t6():
-    return "Take a walk, play an active game"
-
-def t7():
-    return "Prepare and eat lunch, read mail, wash dishes"
-
-def t8():
-    return "Listen to music, do crossword puzzles, watch TV"
-
-def t9():
-    return "Do some gardening, take a walk, visit a friend"
-
-def t10():
-    return "Take a short break or nap"
-
-def t11():
-    return "Prepare and eat dinner, clean up the kitchen"
-
-def t12():
-    return "Reminisce over coffee and dessert"
-
-def t13():
-    return "Play cards, watch a movie, give a massage"
-
-def t14():
-    return "Take a bath, get ready for bed, read a book"
-
-tasks = {0:t0, 1:t1, 2:t2, 3:t3, 4:t4, 5:t5, 6:t6, 7:t7, 8:t8, 9:t9, 10:t10, 11:t11, 12:t12, 13:t13, 14:t14}
+tasks = {0:"Wash, brush teeth, get dressed", 
+1:"Prepare and eat breakfast", 
+2:"Have a conversation over coffee", 
+3:"Discuss the newspaper, try a craft project, reminisce about old photos", 
+4:"Take a break, have some quiet time", 
+5:"Do some chores together", 
+6:"Take a walk, play an active game", 
+7:"Prepare and eat lunch, read mail, wash dishes", 
+8:"Listen to music, do crossword puzzles, watch TV", 
+9:"Do some gardening, take a walk, visit a friend", 
+10:"Take a short break or nap", 
+11:"Prepare and eat dinner, clean up the kitchen", 
+12:"Reminisce over coffee and dessert", 
+13:"Play cards, watch a movie, give a massage",
+14:"Take a bath, get ready for bed, read a book"}
 
 
 def onButtonPush():
@@ -67,14 +43,21 @@ def onButtonPush():
     i = i + 1
     global num
     num = i % NUM_TASKS
-    tsk.set(tasks[num]())
+    tsk.set(tasks[num])
 
-button = tk.Button(app, text="Finished Task", command=onButtonPush, width=30, height=29)
-button.place(x=10, y=10)
+task_label_txt = StringVar()
+task_label_txt.set("Task")
 
-fontStyle = tkFont.Font(family="Open Sans", size = 30)
+task_label = tk.Message(app, textvariable=task_label_txt, font=fontStyle, width=400)
+task_label.config(borderwidth=10, fg='white')
+task_label.place(x=320, y=10)
+
+
+button = tk.Button(app, text="Finished Task", highlightbackground="#90ee90", fg="Black", highlightthickness=200, command=onButtonPush)
+button.place(x=10, y=10, width=300, height=480)
 
 label = tk.Message(app, textvariable=tsk, font=fontStyle, width=400)
-label.place(x=400, y=150)
+label.config(borderwidth=10, fg='black', background='#add8e6')
+label.place(x=350, y=70)
 
 app.mainloop()
